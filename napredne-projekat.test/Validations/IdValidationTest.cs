24f4e4cb3684ed28690269a16cbf0a84a9e4110e
Saponja <jsaponjic512@gmail.com>
2021-06-26 13:03:24 +0200

    Napravljena klasa za validaciju Id-a, napravljen projekat za testiranje i iztestirana klasa IdValidation

---
+++ napredne-projekat.test/Validations/IdValidationTest.cs
@@ -0,0 +1,44 @@
+﻿using napredne_projekat.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace napredne_projekat.test.Validations
+{
+    public class IdValidationTest
+    {
+        private readonly IdValidation idValidation;
+
+        public IdValidationTest()
+        {
+            idValidation = new IdValidation();
+        }
+
+        [Fact]
+        public void IdGraterThenZeroShouldBeValid()
+        {
+            var result = idValidation.IsValid(8);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(-2)]
+        [InlineData(-123)]
+        [InlineData(0)]
+        public void IdLessThenZeroShouldBeInvalid(int id)
+        {
+            var result = idValidation.IsValid(id);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IdNullShouldThrowNullReferenceException()
+        {
+            Assert.Throws<NullReferenceException>(() => idValidation.IsValid(null));
+        }
+
+    }
+}
---
+++ napredne-projekat/Validation/IdValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace napredne_projekat.Validation
+{
+    /// <summary>
+    /// Klasa koja nam sluzi za validaciju Id-a
+    /// </summary>
+    public class IdValidation : ValidationAttribute
+    {
+        /// <summary>
+        /// Metoda koja proverava da li prosledjena vrednost zadovoljava uslove
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns><list type="bullet">
+        /// <item>true - ako zadovoljava uslove</item>
+        /// <item>false - ako ne zadovoljava uslove</item>
+        /// </list></returns>
+        public override bool IsValid(object value)
+        {
+            if(value == null)
+            {
+                throw new NullReferenceException();
+            }
+            if(value is Int32 temp)
+            {
+                if(temp > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
